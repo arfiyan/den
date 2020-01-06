d21d443f9ede19d1c318717200a1cd9b21423748
arfiyan <43879819+arfiyan@users.noreply.github.com>
2020-01-06 12:19:08 +0700

    Create Galau.sh

---
+++ Galau.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+#webdav
+
+clear
+# Variables
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+R='\x1b[1;31m'
+G='\x1b[1;32m'
+B='\x1b[1;34m'
+Y='\x1b[1;33m'
+C='\x1b[1;36m'
+D='\x1b[0m'
+
+
+figlet   "JAWABAN" | lolcat
+
+
+echo "     =================================== $green"| lolcat 
+echo "    | Autor : Raden-Ar $green "| lolcat
+echo "    | Team  : IND CYBER ARMY $green "| lolcat 
+echo "     =================================== $green"| lolcat 
+
+echo ""
+echo ""
+
+echo "Jawaban dari Perasaan Angan" | lolcat
+echo "ini jawaban real " | lolcat
+echo "Jadi Harap siap menerima kenyataan" | lolcat
+echo ""
+echo ""
+
+echo "╭─[Keluhan anda]$green "| lolcat
+read -p "╰─Nama Anda =" target;
+sleep 1
+
+echo "Tunggu.... $green "| lolcat
+sleep 1
+echo "Sudahlah Dia tak mencintaimu lagi$green "| lolcat
+sleep 1
+echo "Dia Benci dengan mu $green "| lolcat
+sleep 1
+echo "jangan trrlalu berharap $green "| lolcat
+sleep 1
+echo "sudah tak ada kesempatan bagimu tuk kembali $green "| lolcat
+sleep 1
+echo "dari awal pun dia tak mencintaimu" | lolcat
+sleep 1
+echo "Biarkan dia bahagia dengan yg lain" | lolcat
+sleep 1
+echo "Dan jngan khawatir dia pasti Baik baik saja"
+
+sleep 5
+echo "Sudah puas Inilah kenyataanya" | lolcat
